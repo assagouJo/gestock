6dae07ad7c4f81ea30447c5f7deadf80c7dfec01
joel assagou <joelassagou81@gmail.com>
2026-02-06 08:50:23 +0000

    some routes 4

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
-  password_hash = db.Column(db.String(128))
+  password_hash = db.Column(db.String(256))
   role = db.Column(db.String(20), nullable=False, default='operateur')
   joined_at = db.Column(db.DateTime(), default = datetime.now(timezone.utc), index = True)
   must_change_password = db.Column(db.Boolean, default=False)
