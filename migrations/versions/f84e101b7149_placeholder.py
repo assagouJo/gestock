44fd92bb8218efdc721be4fca255e4fdc13a59a1
joel assagou <joelassagou81@gmail.com>
2026-02-13 17:18:00 +0000

    restore missing alembic revision

---
+++ migrations/versions/f84e101b7149_placeholder.py
@@ -0,0 +1,18 @@
+"""placeholder revision"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# 🔴 IMPORTANT : Mets exactement l'ID manquant
+revision = 'f84e101b7149'
+down_revision = None  # si tu ne sais pas, mets None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
