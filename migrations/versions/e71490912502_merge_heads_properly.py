9b747c4c76702dd10059b4924ddcf78dd20b4054
joel assagou <joelassagou81@gmail.com>
2026-02-16 09:20:27 +0000

    add merged migration e71490912502

---
+++ migrations/versions/e71490912502_merge_heads_properly.py
@@ -0,0 +1,24 @@
+"""merge heads properly
+
+Revision ID: e71490912502
+Revises: 4aa811145dbe, NOUVEL_ID_ICI
+Create Date: 2026-02-16 09:18:09.455112
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e71490912502'
+down_revision = ('4aa811145dbe', 'NOUVEL_ID_ICI')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
