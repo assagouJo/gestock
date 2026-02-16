8b3e553aa038c734b905275743eb9990ecbf0642
joel assagou <joelassagou81@gmail.com>
2026-02-16 09:13:31 +0000

    mise a jour colonne conditionnement en production

---
+++ migrations/versions/4aa811145dbe_restore_missing_revision.py
@@ -0,0 +1,15 @@
+from alembic import op
+import sqlalchemy as sa
+
+revision = '4aa811145dbe'
+down_revision = None  # IMPORTANT : mets None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
